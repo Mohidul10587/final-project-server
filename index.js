56970a0c139b635b3d2ad951933461d20f302bc6
mohid <mohid10587@gmail.com>
2022-09-19 13:25:06 +0600

    server connected

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+const port =process.env.PORT || 5000
+
+app.use (cors())
+app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://doctors_portal:daLflcZ2ptVWCVSP@cluster0.ysiejik.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+try {
+  await client.connect()
+  console.log('connectted')
+  const serviceCollection = client.db('doctors_portal').collection('services')
+  
+  app.get('/service',async(req,res)=> {
+    const query =   {};
+    const cursor = serviceCollection.find(query);
+    const service = await cursor.toArray();
+    res.send(service)
+  })
+  
+} finally {
+  
+}
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
